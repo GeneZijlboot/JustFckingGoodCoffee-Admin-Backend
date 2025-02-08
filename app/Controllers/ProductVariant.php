cc6179bcaec36fcf0dcec9254f5e0acba08dce8c
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2025-02-08 16:20:32 +0100

    create product variant

--- app/Controllers/ProductVariant.php
+++ app/Controllers/ProductVariant.php
@@ -10,7 +10,7 @@ class ProductVariant extends BaseController
     //constructor function
     public function __construct() {
         //products Model
-        $this->ProductVariant = model(ProductVariants::class);
+        $this->productVariant = model(ProductVariants::class);
     }
 
     //get all users
@@ -24,7 +24,7 @@ class ProductVariant extends BaseController
         $currentUser = $session->get('currentUser');
 
         if ($status = (isset($currentUser) && $currentUser["user_role_id"] == 1)) { // check if a user is logged in and if admin
-            if ($status = $this->ProductVariant->getAll($data)) {
+            if ($status = $this->productVariant->getAll($data)) {
                 foreach ($data as &$product_variant) { // Use a reference to modify the actual array element
                     $product_variant['product_variant_name'] = $product_variant['product_id'] . ' - ' . $product_variant['product_name'];
                     $product_variant['controller'] = 'ProductVariant';
@@ -81,6 +81,49 @@ class ProductVariant extends BaseController
         return $this->response->setJSON($response_data);
     }
 
+    //create Product Variant
+    public function createProductVariant() {
+        //define variables
+        $message = null;
+        $data = [
+            'product_id' => $this->request->getPostGet('product_id'),
+            'product_types' => json_decode($this->request->getPostGet('product_types'), true),
+        ];
+
+        foreach ($data['product_types'] as $product_type) {
+            $newProductType['product_id'] = $data['product_id'];
+            //set price
+            $newProductType['price'] = $product_type['price'];
+        
+            //check if 'weight' is set (since it could be NULL or missing in some cases)
+            if (isset($product_type['weight']) && is_array($product_type['weight'])) {
+                //set weight - assuming 'value' is what you want
+                $newProductType['weight'] = $product_type['weight']['value'];
+            } else {
+                $newProductType['weight'] = NULL;
+            }
+            
+            //post to product_variants table
+            if (!($newProductType['weight'] == NULL || $newProductType['price'] == NULL)) {
+                if ($status = $this->productVariant->insertProductVariant($newProductType)) {
+                    $message = 'succesfully.inserting.product_variant';
+                } else {
+                    $message = 'error.inserting.product_variant';
+                }
+            }
+        }
+
+        //define response data
+        $response_data = [
+            'status' => $status,
+            'data' => $data,
+            'message' => $message
+        ];
+
+        //return response back to frontend -> in JSON format
+        return $this->response->setJSON($response_data);
+    }
+
     public function deleteProductVariant() {
         //define variables
         $message = null;
@@ -93,7 +136,7 @@ class ProductVariant extends BaseController
         $currentUser = $session->get('currentUser');
     
         if ($status = (isset($currentUser) && $currentUser["user_role_id"] == 1)) { // check if a user is logged in and if admin
-            if ($status = $this->ProductVariant->DeleteById($data)) {
+            if ($status = $this->productVariant->DeleteById($data)) {
                 $message = 'succesfully.deleted.product_variant';
             }
         } else {
